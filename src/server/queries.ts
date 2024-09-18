b2cb97cdc4ef13889ac51fdaea15c8d3ddbdf0d3
Francesco Demarco <francescodem402@gmail.com>
2024-09-17 21:01:07 -0400

    Breaking queries into separate file

---
+++ src/server/queries.ts
@@ -0,0 +1,13 @@
+import { auth } from '@clerk/nextjs/server';
+import { eq } from 'drizzle-orm';
+import 'server-only';
+import { db } from "~/server/db";
+export async function getMyImages(){
+    const user=auth();
+    if(!user.userId) throw new Error("Unauthorized")
+    const images = await db.query.images.findMany({
+      where:(model, {eq}) => eq(model.userId, user.userId),
+      orderBy: (model, {desc}) => desc(model.id), 
+     });
+     return images;
+    }
